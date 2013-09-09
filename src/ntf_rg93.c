a9c8ea56b94d24323f1d6d32754bb2c0d49a3189
YaGeek <yannick.heinrich@gmail.com>
2013-09-09 22:50:00 +0200

    Begin with grid library

--- src/ntf_rg93.c
+++ src/ntf_rg93.c
@@ -2,9 +2,14 @@
 #define __LAMBERT_CONFIG_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "ntf_rg93.h"
 #include "config.h"
+#include <zlog.h>
 
+#define MAX_LINE_BUFFER 100
+#define MAX_PATH_BUFFER 100
 /**
 * From : http://geodesie.ign.fr/contenu/fichiers/documentation/algorithmes/notice/NT111_V1_HARMEL_TransfoNTF-RGF93_FormatGrilleNTV2.pdf
 */
@@ -26,9 +31,37 @@ YGLambertPoint intpol_gsb(YGLambertPoint ntf_point)
 
 void test_file()
 {	
-	sprintf("%s/%s",resource_file_path,"gr3df97a.txt");
-	FILE *fd = fopen(resource_file_path)
-	printf("Resource path : %s\n",resource_file_path);
+	char grid_file_path[MAX_PATH_BUFFER];
+	char line_buffer[MAX_LINE_BUFFER];
+
+	snprintf(grid_file_path,50,"%s/%s",resource_file_path,"gr3df97a.txt");
+	
+	FILE *fd = fopen(grid_file_path,"r");
+		
+	if(!fd)
+	{
+		printf("Error could not open grid file");
+		exit(EXIT_FAILURE);
+	}
+
+	int c = 0,index = 0;
+	do
+	{
+		c = fgetc(fd);
+		++index;
+
+		if(index > MAX_LINE_BUFFER || c == '\n')
+		{
+			printf("%s\n",line_buffer);
+			index = 0;
+			memset(line_buffer,0,MAX_LINE_BUFFER);
+		}
+
+		line_buffer[index] = c;
+	}
+	while(c != EOF);
+
+	fclose(fd);
 }
 
 #endif
